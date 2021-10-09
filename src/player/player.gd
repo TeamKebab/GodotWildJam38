1ea324931cbdeaf588a8a15f6261d14103892ca3
Ángela López <angela.lbsr@gmail.com>
2021-10-09 16:09:59 +0200

    player sounds

--- src/player/player.gd
+++ src/player/player.gd
@@ -9,12 +9,31 @@ export var max_fall_speed: float = 1000
 export var jump_speed: float = -800
 
 var motion = Vector2.ZERO
+var was_walking = false
+var was_on_floor = false
 
+onready var walk_sound = $WalkSound
+onready var jump_sound = $JumpSound
+onready var land_sound = $LandSound
 
 func _physics_process(delta: float) -> void:
 	_move(delta)
 	
+	if motion.x != 0 and is_on_floor():
+		if !was_walking:
+			was_walking = true
+			walk_sound.play()
+	else:
+		was_walking = false
+		walk_sound.stop()
 
+	if is_on_floor():
+		if !was_on_floor:
+			land_sound.play()
+			was_on_floor = true
+	else:
+		was_on_floor = false
+		
 func _move(delta):	
 	var x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")	
 	
@@ -27,7 +46,7 @@ func _move(delta):
 	
 	motion = move_and_slide(motion, Vector2.UP)
 	
-	if Input.is_action_just_pressed("ui_up"):		
-		if is_on_floor():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):		
 			motion.y = jump_speed
-	
+			jump_sound.play()

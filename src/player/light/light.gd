a1499249fe5792a849ad93d1de3a783da46a311e
Ángela López <angela.lbsr@gmail.com>
2021-10-09 15:50:01 +0200

    light on off sounds

--- src/player/light/light.gd
+++ src/player/light/light.gd
@@ -1,6 +1,10 @@
 extends Light2D
 
 
+onready var turn_on_sound = $TurnOnSound
+onready var turn_off_sound = $TurnOffSound
+
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		_point_to(event.position)
@@ -8,8 +12,15 @@ func _input(event):
 		if event.is_pressed():
 			_toggle()
 
+
 func _point_to(point: Vector2) -> void:
 	rotation = point.angle_to_point(get_global_transform_with_canvas().origin)
 
+
 func _toggle() -> void:
+	if enabled:
+		turn_off_sound.play()
+	else:
+		turn_on_sound.play()
+		
 	enabled = !enabled

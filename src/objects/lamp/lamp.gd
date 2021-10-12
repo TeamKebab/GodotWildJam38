162b33ab77bb4de014d89102da8302fc5fa5bab6
Ángela López <angela.lbsr@gmail.com>
2021-10-12 13:48:18 +0200

    lamp lit up animation

--- src/objects/lamp/lamp.gd
+++ src/objects/lamp/lamp.gd
@@ -1,48 +1,74 @@
 extends StaticBody2D
 
+export var charge: int = 1
+
 var enabled: bool = true setget _set_enabled
 func _set_enabled(value: bool) -> void:
 	enabled = value
 	
-	for light in lights:
-		light.enabled = value
-#	for light_ball in light_balls:
-#		light_ball.visible = value
+	charging_area.enabled = value
+	
+	if not enabled:
+		area_light.energy = 0
+		bulb_light.energy = 0
 		
-		
-onready var lights = [
-	$Light2D,
-	$Light2D2
-]
 
-onready var light_balls = $Path2D.get_children()
+onready var light_balls_container = $Path2D
+onready var light_balls = light_balls_container.get_children()
 onready var charging_area = $ChargingArea
 onready var timer = $Timer
-onready var turn_on_sound = $TurnOnSound
+onready var animation_player = $AnimationPlayer
 
+onready var area_light = $AreaLight
+onready var bulb_light = $BulbLight
 
 func _ready() -> void:
 	timer.connect("timeout", self, "_on_timeout")
 	_set_enabled(false)
+	light_balls_container.visible = true
 
+	var start_offset = randf()
+	for i in light_balls.size():
+		light_balls[i].unit_offset = start_offset + float(i) / light_balls.size()
+		
 
 func _on_timeout() -> void:
 	if enabled:
 		if !charging_area.bodies_in_detection_area.empty():
-			Game.charge_battery(1)
+			Game.charge_battery(charge)
 
 
+func _input(event: InputEvent) -> void:
+	if $DebugBG.visible:
+		if event is InputEventMouseButton:
+			if  event.pressed:
+				if enabled:
+					_set_enabled(false)
+					for light_ball in light_balls:
+						light_ball.lit_down()
+				else:
+					light_on()
+			else:
+				light_off()
+
+			
 func light_on() -> void:
 	if enabled:
 		return
 	
-	turn_on_sound.play()
+	animation_player.play("lit_up")
 	for light_ball in light_balls:
 		light_ball.lit_up()
-		
-	yield(get_tree().create_timer(1), "timeout")
-	_set_enabled(true)
 
 
 func light_off() -> void:
-	print("lamp unlit")
+	if enabled: 
+		return
+	
+	animation_player.stop()
+	
+	for light_ball in light_balls:
+		light_ball.lit_down()
+		
+	area_light.energy = 0
+	bulb_light.energy = 0
--- src/objects/lamp/light_ball.gd
+++ src/objects/lamp/light_ball.gd
@@ -17,67 +17,84 @@ func _ready() -> void:
 	sprite.modulate.a = 0
 	
 	if $DebugBG.visible:
-		yield(get_tree().create_timer(3), "timeout")
-		lit_up()
+		_debug_animation()
 
 func _physics_process(delta: float) -> void:
 	unit_offset += speed * delta
 	
 
 func lit_up() -> void:
-	for i in 5:
-		_flicker_on()
-		yield(tween, "tween_all_completed")
-		
-		_flicker_off()
-		yield(get_tree().create_timer(0.5 - i / 10.0), "timeout")
-		
-	_flicker_on()
+	_create_lit_up_tween()
+	tween.start()
 
 
-func _flicker_on() -> void:
-	var duration = rand_range(0.2, 0.6)
+func lit_down() -> void:
+	tween.remove_all()
+	light.energy = 0
+	spotlight.energy = 0
+	sprite.modulate.a = 0
+	
+
+func _create_lit_up_tween() -> void:
+	var total_duration = 0
+	
+	for i in 3:
+		var lit_up_duration = rand_range(0.1, 0.4)
+		var lit_down_duration = 0.2
+		var off_duration = 0.3 - i / 10.0
+		
+		_add_light_on_tween(lit_up_duration, total_duration)
+		total_duration += lit_up_duration
+		_add_light_off_tween(lit_down_duration, total_duration)		
+		total_duration += lit_down_duration + off_duration
+		
+	_add_light_on_tween(rand_range(0.2, 0.6), total_duration)	
+
+
+func _add_light_on_tween(duration: float, delay: float) -> void:
 	var duration_offset = 0.1
 	
 	tween.interpolate_property(
 		light, "energy", light.energy, 2,
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		duration_offset
+		delay + duration_offset
 	)
 	tween.interpolate_property(
 		sprite, "modulate:a", sprite.modulate.a, 1,
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		duration_offset
+		delay + duration_offset
 	)
 	tween.interpolate_property(
 		spotlight, "energy", spotlight.energy, 1,
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		0
+		delay + 0
 	)
-	
-	tween.start()
 
 
-func _flicker_off() -> void: 
-	var duration = 0.2
+func _add_light_off_tween(duration: float, delay: float) -> void: 
 	var duration_offset = 0.1
 	
 	tween.interpolate_property(
 		light, "energy", 2, 0,
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		duration_offset
+		delay + duration_offset
 	)
 	tween.interpolate_property(
 		sprite, "modulate:a", 1, 0,
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		duration_offset
+		delay + duration_offset
 	)
 	tween.interpolate_property(
 		spotlight, "energy", 1.5, 0, 
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN,
-		0
+		delay + 0
 	)
-	
-	tween.start()
-	
+
+
+func _debug_animation() -> void:
+	yield(get_tree().create_timer(3), "timeout")
+	lit_up()
+	yield(get_tree().create_timer(2), "timeout")
+	lit_down()	
+	_debug_animation()
 

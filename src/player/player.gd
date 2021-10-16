f6fe43bce4ecd0df30a509b4bdde248cc11b3eba
Ángela López <angela.lbsr@gmail.com>
2021-10-16 15:43:50 +0200

    player animations

--- src/player/player.gd
+++ src/player/player.gd
@@ -7,14 +7,34 @@ onready var motion = $Motion
 onready var scare_sound = $ScareSound
 onready var damage_box = $DamageBox
 
+onready var animationTree = $AnimationTree
+onready var facing_direction_animation = animationTree.get("parameters/facing_direction/playback")
+onready var action_animation = animationTree.get("parameters/action/playback")
+
 
 func _ready() -> void:
 	Game.player = self
 	Game.player_camera = $Camera2D
 	
-func _physics_process(delta: float) -> void:
-	motion.move(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), delta)
+	animationTree.active = true
 	
+	
+func _physics_process(delta: float) -> void:
+	var input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	motion.move(input, delta)
+	
+	if input < 0:
+		facing_direction_animation.travel("face_left")
+	elif input > 0:
+		facing_direction_animation.travel("face_right")
+	
+	if motion.motion.y > 0:
+		action_animation.travel("fall")
+	elif motion.motion.y == 0 and motion.motion.x != 0:
+		action_animation.travel("walk")
+	else:
+		action_animation.travel("idle")
+		
 	if Input.is_action_just_pressed("ui_up"):
 		motion.jump()
 	

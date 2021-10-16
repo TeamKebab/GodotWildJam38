fd771c148513dcdded007802520003f169a1421a
Ángela López <angela.lbsr@gmail.com>
2021-10-16 12:21:47 +0200

    lit up portal

--- src/objects/portal/portal.gd
+++ src/objects/portal/portal.gd
@@ -1,11 +1,37 @@
-extends Area2D
+extends StaticBody2D
 
 
 export(Game.Scene) var scene = Game.Scene.Level_01
 
+
+onready var collision = $CollisionShape2D
+onready var teleportArea = $TeleportArea
+onready var animationPlayer = $AnimationPlayer
+onready var chargingSound = $ChargingSound
+
+
 func _ready() -> void:
-	connect("area_entered", self, "_on_player_entered")
+	_set_portal_active(false)
+	teleportArea.connect("area_entered", self, "_on_player_entered")
 
+func light_on() -> void:
+	animationPlayer.play("charging")
+	chargingSound.play()
 
+ 
+func light_off() -> void:
+	animationPlayer.play("idle")
+	chargingSound.stop()
+	
+
+func _set_portal_active(value: bool) -> void:
+	teleportArea.get_child(0).disabled = !value
+	collision.disabled = value
+		
+
+func _on_charged() -> void:
+	_set_portal_active(true)
+	
+	
 func _on_player_entered(_player_damage_box: Area2D) -> void:
 	Game.go_to(scene)

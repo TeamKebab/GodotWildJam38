6a32ba0ae2be6be14e19c4c2608b7f4114516baf
Ángela López <angela.lbsr@gmail.com>
2021-10-17 18:06:12 +0200

    spoopy identified sound

--- src/spoopies/states/identified.gd
+++ src/spoopies/states/identified.gd
@@ -1,9 +1,20 @@
 extends "res://spoopies/states/walking.gd"
 
+const sounds = [
+	preload("res://spoopies/spoopy_unspoopied.wav"),
+	preload("res://spoopies/spoopy_unspoopied2.wav"),
+	preload("res://spoopies/spoopy_unspoopied4.wav"),
+	preload("res://spoopies/spoopy_unspoopied5.wav"),
+]
+
 onready var animationStatus = animationTree.get("parameters/status/playback")
+onready var sound = spoopy.find_node("AudioStreamPlayer2D")
+
 
 func enter() -> void:
 	Game.identified_spoopies += 1
+	sound.stream = sounds[randi() % sounds.size()]
+	sound.play()
 	spoopy.identify()
 	animationStatus.travel("identified")
 	face(0)
